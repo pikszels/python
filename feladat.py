d9184dd7583ba7fec0705a9bc0758139246bc12c
pikszels <pappzg@kkszki.hu>
2025-11-27 16:58:11 +0100

    sixth task

--- feladat.py
+++ feladat.py
@@ -1,19 +1,22 @@
 #1
-""" szam1 = input("Adj meg egy számot: ")
+""" print("1. feladat")
+szam1 = input("Adj meg egy számot: ")
 szam2 = input("Adj meg egy másik számot: ")
 szam1 = int(szam1)
 szam2 = int(szam2)
 print(f"A két szám összege: {szam1 + szam2}") """
 
 #2
-""" szam = input("Adj meg egy számot: ")
+""" print("2. feladat")
+szam = input("Adj meg egy számot: ")
 if int(szam) % 2 == 0:
     print("Páros szám")
 else:
     print("Páratlan szám") """
 
 #3
-""" szam1 = input("Adj meg egy számot: ")
+""" print("3. feladat")
+szam1 = input("Adj meg egy számot: ")
 szam2 = input("Adj meg egy másik számot: ")
 szam3 = input("Adj meg egy harmadik számot: ")
 szam1 = int(szam1)
@@ -23,8 +26,29 @@ legnagyobb = max(szam1, szam2, szam3)
 print(f"A legnagyobb szám: {legnagyobb}") """
 
 #4
-""" n = input("Adj meg egy számot: ")
+""" print("\n 4. feladat")
+n = input("Adj meg egy számot: ")
 n = int(n)
 print(f"A számok 1-től {n}-ig:")
 for i in range(1, n + 1):
     print(i, end = " ") """
+
+#5
+""" print("\n 5. feladat")
+
+szamok_listaja = []
+db = 5
+
+# Kérj be 5 számot egy ciklus segítségével
+for i in range(db):
+    while True:
+        try:
+            szam_input = float(input(f"Kérlek, add meg a(z) {i + 1}. számot: "))
+            szamok_listaja.append(szam_input)
+            break
+        except ValueError:
+            print("Hibás bevitel. Kérlek, érvényes számot adj meg.")
+
+osszeg = sum(szamok_listaja)
+atlag = osszeg / len(szamok_listaja)
+print(f"A számok átlaga: {atlag:.1f}") """
