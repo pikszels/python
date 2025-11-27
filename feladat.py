25de0071b5a3ebe12e8f377789d68e1bc91f86df
pikszels <pappzg@kkszki.hu>
2025-11-27 16:29:03 +0100

    third task

--- feladat.py
+++ feladat.py
@@ -11,3 +11,13 @@ if int(szam) % 2 == 0:
     print("Páros szám")
 else:
     print("Páratlan szám") """
+
+#3
+""" szam1 = input("Adj meg egy számot: ")
+szam2 = input("Adj meg egy másik számot: ")
+szam3 = input("Adj meg egy harmadik számot: ")
+szam1 = int(szam1)
+szam2 = int(szam2)
+szam3 = int(szam3)
+legnagyobb = max(szam1, szam2, szam3)
+print(f"A legnagyobb szám: {legnagyobb}") """
