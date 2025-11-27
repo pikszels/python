8e03763e8f4f41a9726b00a6ff174076826957b3
pikszels <pappzg@kkszki.hu>
2025-11-27 18:43:03 +0100

    seventh task

---
+++ feladat7.py
@@ -0,0 +1,7 @@
+print("\n 7. feladat")
+
+szlista = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+legnagyobb = max(szlista)
+legkisebb = min(szlista)
+print(f"A legnagyobb elem: {legnagyobb}")
+print(f"A legkisebb elem: {legkisebb}")
