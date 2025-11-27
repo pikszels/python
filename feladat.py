5b634703a86d1b05fe2028fd036fa82ddf40d235
pikszels <pappzg@kkszki.hu>
2025-11-27 17:46:27 +0100

    eighth task

--- feladat.py
+++ feladat.py
@@ -69,9 +69,10 @@ print(f"A legnagyobb elem: {legnagyobb}")
 print(f"A legkisebb elem: {legkisebb}") """
 
 #8
+""" print("\n 8. feladat")
 lista = [10, 6, 8, 68, 7, 11, 76, 84, 51]
 szam = int(input ("Adj meg egy számot: "))
 if szam in lista:
     print("A szám megtalálható a listában")
 else:print("A szám nincs a listában")
-print(f"A számok listája: {lista}")
+print(f"A számok listája: {lista}") """
